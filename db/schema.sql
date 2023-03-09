e54c4ad56ccc8d1af63e6bdd140d0000fe082a49
Xavier Teo <xvrteo@gmail.com>
2023-03-09 01:42:25 -0800

    Added data to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department
+(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR '30' NOT NULL
+);
+
+CREATE TABLE roles
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR("SELECT") NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY 'department_id' REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee
+(
+  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INT UNSIGNED,
+  CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
+  manager_id INT,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
